54a8f307e65d4b8ff6da8adb83593c519c87a1a8
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-05 23:14:57 +0700

    snackbar_test.dart - ready to test

---
+++ ruangpeduliapp/test/widget/panti/homepage/components/snackbar_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('SnackBar show test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Berhasil'),
+                    ),
+                  );
+                },
+                child: const Text('Show Snackbar'),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('Show Snackbar'), findsOneWidget);
+
+    await tester.tap(find.text('Show Snackbar'));
+    await tester.pump(); // trigger animation start
+    await tester.pump(const Duration(seconds: 1)); // show snackbar
+
+    expect(find.text('Berhasil'), findsOneWidget);
+  });
+}
