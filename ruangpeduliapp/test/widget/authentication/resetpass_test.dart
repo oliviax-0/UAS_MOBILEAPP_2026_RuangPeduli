bc7e9d8ea2700c2b40d2ce0fcebffacb6c869e71
Olivia Norine <oliv@Olivs-Mac.local>
2026-05-06 00:32:47 +0700

    resr_pass widget ready to test 1

--- ruangpeduliapp/test/widget/authentication/resetpass_test.dart
+++ ruangpeduliapp/test/widget/authentication/resetpass_test.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ruangpeduliapp/auth/reset_password_new_screen.dart';
 import '../shared/screen_builders.dart';
 
 void main() {
