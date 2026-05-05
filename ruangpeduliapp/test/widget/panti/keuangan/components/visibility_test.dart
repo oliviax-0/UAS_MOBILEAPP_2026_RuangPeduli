05b3ddfa1b1598353a4a6080f74489fce036db05
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-06 00:24:38 +0700

    visibility_test.dart - ready to test

---
+++ ruangpeduliapp/test/widget/panti/keuangan/components/visibility_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/keuangan_panti/keuangan_panti.dart';
+
+void main() {
+  testWidgets('Visibility toggle test', (WidgetTester tester) async {
+    bool isVisible = true;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: StatefulBuilder(
+          builder: (context, setState) {
+            return Scaffold(
+              body: DashCard(
+                title: 'Saldo',
+                amount: 100000.0,
+                isVisible: isVisible,
+                onToggle: () {
+                  setState(() {
+                    isVisible = !isVisible;
+                  });
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    // Awalnya visible
+    expect(find.byIcon(Icons.visibility), findsOneWidget);
+
+    // Tap toggle
+    await tester.tap(find.byIcon(Icons.visibility));
+    await tester.pump();
+
+    // Setelah toggle jadi hidden
+    expect(find.byIcon(Icons.visibility_off), findsOneWidget);
+  });
+}
