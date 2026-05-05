f5530f9c30f31b6256fa564cd3f3d271b8e6fb21
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-06 00:16:04 +0700

    tab_test.dart - ready to test

---
+++ ruangpeduliapp/test/widget/panti/keuangan/components/tab_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/keuangan_panti/keuangan_plus.dart';
+
+void main() {
+  testWidgets('Tab switch render & tap test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: InputTransaksiPage(
+          userId: 1,
+          pantiId: 1,
+        ),
+      ),
+    );
+
+    expect(find.text('Pemasukan'), findsOneWidget);
+    expect(find.text('Pengeluaran'), findsOneWidget);
+
+    await tester.tap(find.text('Pengeluaran'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(InputTransaksiPage), findsOneWidget);
+  });
+}
