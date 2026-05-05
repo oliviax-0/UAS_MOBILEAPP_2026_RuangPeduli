a5e530440df569bcd508bf7fd481fe1554b64169
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-06 00:20:04 +0700

    date_test.dart - ready to test

---
+++ ruangpeduliapp/test/widget/panti/keuangan/components/date_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/keuangan_panti/keuangan_plus.dart';
+
+void main() {
+  testWidgets('Date picker open test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: InputTransaksiPage(
+          userId: 1,
+          pantiId: 1,
+        ),
+      ),
+    );
+
+    final dateField = find.textContaining('Tanggal').evaluate().isNotEmpty
+        ? find.textContaining('Tanggal')
+        : find.byType(GestureDetector);
+
+    if (dateField.evaluate().isNotEmpty) {
+      await tester.tap(dateField.first);
+      await tester.pumpAndSettle();
+    }
+
+    expect(find.byType(InputTransaksiPage), findsOneWidget);
+  });
+}
