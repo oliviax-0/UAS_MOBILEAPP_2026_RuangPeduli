2ff428b7f1b3f815457c5eae0812cbe71176550d
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-05 23:12:54 +0700

    fab_test - ready to test

---
+++ ruangpeduliapp/test/widget/panti/homepage/components/fab_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('FAB render & tap test', (WidgetTester tester) async {
+    bool tapped = false;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              tapped = true;
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+
+    expect(tapped, true);
+  });
+}
