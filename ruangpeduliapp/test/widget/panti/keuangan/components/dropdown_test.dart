bbb410031e62772395fe37c8593ba0efa35a7a55
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-06 00:17:28 +0700

    dropdown_test.dart - ready to test 1

---
+++ ruangpeduliapp/test/widget/panti/keuangan/components/dropdown_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/keuangan_panti/keuangan_plus.dart';
+
+void main() {
+  testWidgets('Dropdown render & select test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: InputTransaksiPage(
+          userId: 1,
+          pantiId: 1,
+        ),
+      ),
+    );
+
+    final dropdown = find.byType(DropdownButton);
+    expect(dropdown, findsWidgets);
+
+    // Buka dropdown pertama
+    if (dropdown.evaluate().isNotEmpty) {
+      await tester.tap(dropdown.first);
+      await tester.pumpAndSettle();
+
+      // Pilih item pertama jika ada
+      final item = find.byType(DropdownMenuItem);
+
+      if (item.evaluate().isNotEmpty) {
+        await tester.tap(item.first);
+        await tester.pumpAndSettle();
+      }
+    }
+
+    expect(find.byType(InputTransaksiPage), findsOneWidget);
+  });
+}
