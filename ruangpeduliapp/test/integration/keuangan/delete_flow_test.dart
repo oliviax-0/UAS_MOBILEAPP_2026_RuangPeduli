5cbbf5772e49f07b8b5e6cfba3a2f7df4248309b
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-06 00:29:41 +0700

    delete_flow_test.dart - ready to test

---
+++ ruangpeduliapp/test/integration/keuangan/delete_flow_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/keuangan_panti/keuangan_panti.dart';
+
+void main() {
+  testWidgets('Delete transaction flow test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: KeuanganPanti(
+          userId: null,
+          pantiId: null,
+        ),
+      ),
+    );
+
+    expect(find.byType(KeuanganPanti), findsOneWidget);
+
+    await tester.pump(const Duration(milliseconds: 300));
+
+    final trxTile = find.byType(TrxTile);
+
+    if (trxTile.evaluate().isNotEmpty) {
+      await tester.tap(trxTile.first);
+      await tester.pump();
+
+      final snackbar = find.byType(SnackBar);
+
+      expect(
+        snackbar.evaluate().isNotEmpty ||
+            find.byType(KeuanganPanti).evaluate().isNotEmpty,
+        true,
+      );
+    } else {
+      expect(find.byType(KeuanganPanti), findsOneWidget);
+    }
+  });
+}
