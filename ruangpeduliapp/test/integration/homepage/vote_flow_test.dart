390c78b6a0fe830af10becb7d4330d72672db6c7
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-05 23:34:27 +0700

    vote_flow_test.dart - ready to test

---
+++ ruangpeduliapp/test/integration/homepage/vote_flow_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/home_panti/home_berita_panti.dart';
+
+void main() {
+  testWidgets('Vote flow basic test', (WidgetTester tester) async {
+    int up = 0;
+    int down = 0;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Column(
+            children: [
+              VoteButton(
+                isUpvote: true,
+                count: up,
+                onTap: () {
+                  up++;
+                },
+              ),
+              VoteButton(
+                isUpvote: false,
+                count: down,
+                onTap: () {
+                  down++;
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    // Pastikan tombol ada
+    expect(find.byIcon(Icons.thumb_up), findsOneWidget);
+    expect(find.byIcon(Icons.thumb_down), findsOneWidget);
+
+    // Tap upvote
+    await tester.tap(find.byIcon(Icons.thumb_up));
+    await tester.pump();
+    expect(up, 1);
+
+    // Tap downvote
+    await tester.tap(find.byIcon(Icons.thumb_down));
+    await tester.pump();
+    expect(down, 1);
+  });
+}
