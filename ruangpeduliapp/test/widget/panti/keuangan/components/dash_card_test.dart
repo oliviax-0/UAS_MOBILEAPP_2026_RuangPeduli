e2d72fde66f52d401bbfbda530f02882a1676f48
ValentinoRicciMahawi <mahawivalentinoricci@gmail.com>
2026-05-06 00:13:12 +0700

    dash_card_test.dart - ready to test

---
+++ ruangpeduliapp/test/widget/panti/keuangan/components/dash_card_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/panti/keuangan_panti/keuangan_panti.dart';
+
+void main() {
+  testWidgets('DashCard render test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: KeuanganPanti(
+            userId: null,
+            pantiId: null,
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byType(KeuanganPanti), findsOneWidget);
+    expect(find.textContaining('Saldo'), findsWidgets);
+
+    expect(find.byIcon(Icons.visibility_outlined), findsOneWidget);
+  });
+}
