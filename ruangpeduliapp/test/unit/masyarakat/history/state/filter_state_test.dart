666cbcd0e4154ad4b73baf42caa5e0a3e17ee943
andre <andrenaldy05@gmail.com>
2026-05-06 16:52:48 +0700

    unit history ready to be tested

---
+++ ruangpeduliapp/test/unit/masyarakat/history/state/filter_state_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Filter state', () {
+    test('selected date label is formatted correctly', () {
+      // TODO: verify filter label formatting logic and date selection
+    });
+
+    test('date selection updates filter state', () {
+      // TODO: simulate selecting date and assert callback behavior
+    });
+  });
+}
