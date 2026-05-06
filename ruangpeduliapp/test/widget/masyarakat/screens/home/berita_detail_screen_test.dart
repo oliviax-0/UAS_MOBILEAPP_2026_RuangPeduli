770ff12d26e2fdbc28f102f7bc05d45b56248ec8
andre <andrenaldy05@gmail.com>
2026-05-06 10:14:10 +0700

    bertia_detail_screen_test.dart ready to be tested

---
+++ ruangpeduliapp/test/widget/masyarakat/screens/home/berita_detail_screen_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ruangpeduliapp/masyarakat/home/berita_detail_screen.dart';
+
+void main() {
+  group('BeritaDetailScreen Tests', () {
+    testWidgets('BeritaDetailScreen renders with berita data', (WidgetTester tester) async {
+      // TODO: Build widget with berita data
+      // await tester.pumpWidget(const MaterialApp(
+      //   home: BeritaDetailScreen(beritaId: 1),
+      // ));
+
+      // TODO: Verify berita details are displayed
+    });
+
+    testWidgets('Display berita title', (WidgetTester tester) async {
+      // TODO: Test title display
+    });
+
+    testWidgets('Display berita feature image', (WidgetTester tester) async {
+      // TODO: Test image display
+    });
+
+    testWidgets('Display berita publication date', (WidgetTester tester) async {
+      // TODO: Test date display
+    });
+
+    testWidgets('Display berita author', (WidgetTester tester) async {
+      // TODO: Test author display
+    });
+
+    testWidgets('Display full berita content', (WidgetTester tester) async {
+      // TODO: Test content display with markdown rendering
+    });
+
+    testWidgets('Display category/tags', (WidgetTester tester) async {
+      // TODO: Test category display
+    });
+
+    testWidgets('Share berita functionality', (WidgetTester tester) async {
+      // TODO: Test share action
+    });
+
+    testWidgets('Like/bookmark berita', (WidgetTester tester) async {
+      // TODO: Test like functionality
+    });
+
+    testWidgets('Display related berita suggestions', (WidgetTester tester) async {
+      // TODO: Test related articles display
+    });
+
+    testWidgets('Navigate to related berita', (WidgetTester tester) async {
+      // TODO: Test navigation to related berita
+    });
+
+    testWidgets('Handle image loading errors', (WidgetTester tester) async {
+      // TODO: Test error placeholder
+    });
+  });
+}
